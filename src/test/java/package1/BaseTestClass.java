4969afd3c68347094e8141127a85e3772d02f9a7
raheela974 <raheela974@gmail.com>
2025-07-14 19:18:37 +0530

    first commit

---
+++ src/test/java/package1/BaseTestClass.java
@@ -0,0 +1,24 @@
+package package1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTestClass {
+	
+	@Test
+	public void testMethod() {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		
+		Assert.assertEquals(driver.getCurrentUrl(), "https://rahulshettyacademy.com/locatorspractice/");
+		driver.quit();
+	}
+
+}
+
