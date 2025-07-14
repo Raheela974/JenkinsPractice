64523a21f47af27547b06e3206cbb5e06228c0a2
Raheela974 <raheelab845@gmail.com>
2025-07-14 20:52:49 +0530

    Update BaseTestClass.java

--- src/test/java/package1/BaseTestClass.java
+++ src/test/java/package1/BaseTestClass.java
@@ -11,7 +11,7 @@ public class BaseTestClass {
 	
 	@Test
 	public void testMethod() {
-		//test comments
+		//test comment
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
